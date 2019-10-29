3ea8fb06e8a900155e0459309614311cb5d3cc3a
Daniel <sherman2017meizu@gmail.com>
2019-10-29 20:40:32 +0200

    bugs fixed

--- lab_4.py
+++ lab_4.py
@@ -1,12 +1,9 @@
 import math
-
-def func(n):
-    y = y_0 = math.sqrt(3 * n)
-
-    for i in range(n-1, 0, -1):
-        y = math.sqrt(3*i + y_0)
-        y_0 = y
+def root_sequence(n):
+    y = math.sqrt(3 * n)
+    for i in range(n - 1, 0, -1):
+        y = math.sqrt(3*i + y)
     return y
 
 n = int(input('n: '))
-print(func(n))
+print(root_sequence(n))
