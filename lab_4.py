62b77a6829d0488b7564e1d9d2296b7c84bb5e90
Daniel <sherman2017meizu@gmail.com>
2019-10-30 07:45:31 +0200

    bugs fixed #2

--- lab_4.py
+++ lab_4.py
@@ -2,7 +2,7 @@ import math
 def root_sequence(n):
     y = math.sqrt(3 * n)
     for i in range(n - 1, 0, -1):
-        y = math.sqrt(3*i + y)
+        y = math.sqrt(3 * i + y)
     return y
 
 n = int(input('n: '))
