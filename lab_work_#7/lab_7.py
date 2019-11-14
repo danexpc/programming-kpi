35e4cb934631d9994df2b7712bda4b9284b6eae5
Danil <sherman2017meizu@gmail.com>
2019-11-14 19:09:49 +0200

    New laboratory works

---
+++ lab_work_#7/lab_7.py
@@ -0,0 +1,40 @@
+import random
+
+def array_creation(n, a, b):
+    array = [random.randint(a, b) for i in range(n)]
+    return array
+
+def dictionary_creation(array):
+    dictionary = {}
+    for i in arr:
+        dictionary[i] = array.count(i)
+    return dictionary
+
+def appear_the_most(dictionary, max_number):
+    min_key = max_number
+    max_value = max(dictionary.values())
+
+    for k, v in dictionary.items():
+        if v == max_value and min_key > k:
+            min_key = k
+
+    return min_key
+
+n = int(input("Введите размер массива: "))
+
+print("Задайте диапазон целых чисел в массиве: ")
+a = int(input("От: "))
+b = int(input("До: "))
+arr = array_creation(n, a, b)
+print(arr)
+
+dictionary = dictionary_creation(arr)
+del arr
+
+popular_number = appear_the_most(dictionary, b)
+print(F'Наиболее встречаемое число -- {popular_number}')
+
+
+
+
+
